7093ae609b274c3885e9feab45423ba2260a9482
Ancil-Benny <ancilbennyka@gmail.com>
2025-03-02 22:21:39 +0530

    add entry scripts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-import PreloadScene from './scenes/PreloadScene.js';
-import MenuScene from './scenes/MenuScene.js';
-import GameScene from './scenes/GameScene.js';
-
-// Phaser game configuration object
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: "#000",
-    scene: [PreloadScene, MenuScene, GameScene]
-};
-
-// Create a new Phaser Game instance with the specified configuration
-const game = new Phaser.Game(config);
+$(document).ready(function() {
+    // Initialize game once DOM is fully loaded
+    Game.init();
+});
